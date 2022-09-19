0a9965882e5053b79745d05b11d6798e5a9ec1fb
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2022-09-19 08:32:50 +0200

    Test ZIO with Scope in environment

--- scalafix/input/src/main/scala/test/TestZIOService.scala
+++ scalafix/input/src/main/scala/test/TestZIOService.scala
@@ -9,6 +9,7 @@ trait TestZIOService {
   def testTask(i: Int): Task[Int]
   def testIO(i: Int): IO[Throwable, Int]
   def test(i: Int): ZIO[Any, Throwable, Int]
+  def testWithScope(i: Int): ZIO[Scope, Throwable, Int]
   def test2(i: Int): ZIO[Any, Throwable, Int]
 }
 
--- scalafix/output/src/main/scala/test/TestZIOService.scala
+++ scalafix/output/src/main/scala/test/TestZIOService.scala
@@ -6,11 +6,13 @@ trait TestZIOService {
   def testTask(i: Int): Task[Int]
   def testIO(i: Int): IO[Throwable, Int]
   def test(i: Int): ZIO[Any, Throwable, Int]
+  def testWithScope(i: Int): ZIO[Scope, Throwable, Int]
   def test2(i: Int): ZIO[Any, Throwable, Int]
 }
 object TestZIOService {
   def testTask(i: Int): RIO[TestZIOService, Int] = ZIO.serviceWithZIO[TestZIOService](_.testTask(i))
   def testIO(i: Int): ZIO[TestZIOService, Throwable, Int] = ZIO.serviceWithZIO[TestZIOService](_.testIO(i))
   def test(i: Int): ZIO[TestZIOService, Throwable, Int] = ZIO.serviceWithZIO[TestZIOService](_.test(i))
+  def testWithScope(i: Int): ZIO[TestZIOService with Scope, Throwable, Int] = ZIO.serviceWithZIO[TestZIOService](_.testWithScope(i))
   def test2(i: Int): ZIO[TestZIOService, Throwable, Int] = ZIO.serviceWithZIO[TestZIOService](_.test2(i))
 }
